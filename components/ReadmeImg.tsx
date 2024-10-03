08ae6203aae702c561f8db3ed356a3764130b3f0
harryobrien <harry@o-brien.me>
2024-10-03 16:13:02 +0100

    remove svgification

--- components/ReadmeImg.tsx
+++ components/ReadmeImg.tsx
@@ -2,24 +2,17 @@ import React from "react";
 
 const ReadmeImg = ({ width, height, children }) => {
   return (
-    <svg
-      fill="none"
-      width={width}
-      height={height}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <foreignObject width={width} height={height}>
-        <div {...{ xmlns: "http://www.w3.org/1999/xhtml" }}>
-          <style>{`
+    <foreignObject width={width} height={height}>
+      <div {...{ xmlns: "http://www.w3.org/1999/xhtml" }}>
+        <style>{`
               * {
                 margin: 0;
                 box-sizing: border-box;
               }
             `}</style>
-          {children}
-        </div>
-      </foreignObject>
-    </svg>
+        {children}
+      </div>
+    </foreignObject>
   );
 };
 
